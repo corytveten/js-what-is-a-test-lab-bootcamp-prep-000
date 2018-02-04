77549328129be471dbbd5c2101efeff70e2b0e39
corytveten <corytveten@gmail.com>
2018-02-04 16:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ var height = 37
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+console.log("Hello, world!")
+
+function sayHello() {
+  console.log("Hello!")
+}
+sayHello()
+function sayHelloToIsabel() {
+  console.log("Hello, Isabel!")
+}
+
+function sayHelloToSofia() {
+  console.log("Hello, Sofia!")
+}
+
+function SayHelloToBrendan() {
+  console.log("Hello, Brendan!")
+}
+
+sayHelloToIsabel()
+sayHelloToSofia()
+
+function doSomething(thing) {
+  console.log(thing)
+}
+
+doSomething("anything")
